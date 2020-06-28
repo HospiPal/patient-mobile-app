881db0edce399b273c47232ced5aa795ea6c96a9
Kenny Newman <knewman55@gmail.com>
2020-06-27 23:51:26 -0400

    Button Widget
    Signed-off-by: Kenny <knewman55@gmail.com>

--- lib/auth/ui/auth.dart
+++ lib/auth/ui/auth.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:patientapp/auth/login/ui/login.dart';
 import 'package:patientapp/auth/register/ui/register.dart';
-import '../../navigation.dart';
 
 class Auth extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Login();
+    return Register();
   }
 }
 
@@ -27,8 +25,7 @@ class authButton extends StatelessWidget {
       splashColor: Colors.black,
       textColor: Colors.white,
       onPressed: () {
-        Navigator.pushNamed(
-            context, route);
+        Navigator.pushReplacementNamed(context, route);
       },
       child: Text(title),
     );
