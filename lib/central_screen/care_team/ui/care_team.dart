beb023ff33fc954b50ca64c30775730addf055d9
Ethan Sawyer <ethan.b.sawyer@gmail.com>
2020-06-30 23:24:05 -0500

    Created cards for the team function.
    Signed-off-by: Ethan Sawyer <ethan.b.sawyer@gmail.com>

--- lib/central_screen/care_team/ui/care_team.dart
+++ lib/central_screen/care_team/ui/care_team.dart
@@ -9,29 +9,60 @@ class _CareTeamState extends State<CareTeam> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(actions: <Widget>[
-          FlatButton(
-            textColor: Colors.white,
-            child: Text('Logout'),
-            onPressed: () {},
-          )
-        ]),
-        body: SafeArea(
-          child: Center(
-            child: GridView.count(
-                padding: const EdgeInsets.all(20),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                crossAxisCount: 2,
-                children: List.generate(20, (index) {
-                  return Container(
-                    padding: const EdgeInsets.all(8),
-                    child: CircleAvatar(
-                      child: Text('$index'),
-                    ),
-                  );
-                })),
+      appBar: AppBar(actions: <Widget>[
+        FlatButton(
+          textColor: Colors.white,
+          child: Text('Logout'),
+          onPressed: () {},
+        )
+      ]),
+      body: SafeArea(
+        child: Center(
+          child: ListView.builder(
+            scrollDirection: Axis.vertical,
+            padding: const EdgeInsets.all(10),
+            itemCount: 4,
+            itemBuilder: (context, index) {
+              //TODO: look into resizing the card
+              return Card(
+                child: Column(
+                  children: <Widget>[
+                    //TODO: look into button bar theme
+                    ButtonBar(
+                      alignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.face),
+                          onPressed: () {},
+                        ),
+                        IconButton(icon: Icon(Icons.message), onPressed: () {}),
+                        IconButton(
+                          icon: Icon(Icons.phone),
+                          onPressed: () {},
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            },
           ),
-        ));
+        ),
+
+//            child: GridView.count(
+//                padding: const EdgeInsets.all(20),
+//                crossAxisSpacing: 10,
+//                mainAxisSpacing: 10,
+//                crossAxisCount: 2,
+//                children: List.generate(20, (index) {
+//                  return Container(
+//                    padding: const EdgeInsets.all(8),
+//                    child: CircleAvatar(
+//                      child: Text('$index'),
+//                    ),
+//                  );
+//                })),
+      ),
+    );
   }
 }
