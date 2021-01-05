a62051fbbe84a01549f3610fe94ac1de4714f9c3
Kenny Newman <knewman55@gmail.com>
2021-01-05 13:00:50 -0500

    Added to medication calendar
    Signed-off-by: Kenny <knewman55@gmail.com>

--- lib/views/central_screen/records/medications/medications.dart
+++ lib/views/central_screen/records/medications/medications.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:patientapp/views/central_screen/records/medications/medication_info.dart';
@@ -18,19 +17,36 @@ class _MedicationsState extends State<Medications> {
     MedicationInfo(
       name: 'Tylenol',
       daysToTake: <int>[1, 3, 4, 5],
+      amount: 2,
+      form: 'pills',
     ),
     MedicationInfo(
       name: 'heroin',
       daysToTake: <int>[0, 2, 3, 4, 5],
+      amount: 2,
+      form: 'Mg',
     ),
     MedicationInfo(
       name: 'Beta Blocker',
       daysToTake: <int>[1, 3, 5],
+      amount: 2,
+      form: 'pills',
     ),
     MedicationInfo(
       name: 'Insulin',
       daysToTake: <int>[0, 6],
+      amount: 2,
+      form: 'pills',
     ),
+  ];
+  final List<String> daysOfTheWeek = <String>[
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
   ];
   int selectedDay = -1; //what day is selected
 
@@ -51,15 +67,21 @@ class _MedicationsState extends State<Medications> {
             ),
             medTable(),
             Container(
-              height: 100,
+              height: 50,
               //color: Colors.amber[500],
               //child: const Center(child: Text('Entry B')),
             ),
             Container(
               height: 50,
-              color: Colors.amber[100],
-              child: const Center(child: Text('Entry C')),
+              //color: Colors.amber[100],
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    daysOfTheWeek[selectedDayChecker(selectedDay)],
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  )),
             ),
+            medInfoList(),
           ],
         ),
       ),
@@ -103,6 +125,7 @@ class _MedicationsState extends State<Medications> {
       rowOfBoxes[i] = new Container(
         child: Text(''),
         width: 50,
+        padding: EdgeInsets.all(2.0),
         //height: 50,
       );
     }
@@ -111,24 +134,28 @@ class _MedicationsState extends State<Medications> {
       //colored if medication should be taken
       if (medication.daysToTake[i] != 7) {
         rowOfBoxes[medication.daysToTake[i]] = new Container(
-          child: Text(''),
           height: boxHeight,
+          //padding: EdgeInsets.fromLTRB(0, 0.5, 0, 0.5),
           decoration: BoxDecoration(
             border: Border(
                 bottom: BorderSide(
-                  color: Colors.black,
-                )
-            ),
+              color: Colors.black,
+            )),
             color: col,
           ),
         );
       } else {
+        //for sunday since it DateTime stores it as 7 instead of 0
         rowOfBoxes[medication.daysToTake[0]] = new Container(
           color: col,
           height: boxHeight,
-          child: Text(''),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+          ),
+          //padding: EdgeInsets.fromLTRB(0, 0.5, 0, 0.5),
         );
       }
+
       if (columnBordered < 0) {
         //will be -1 by default so the default selected is the given day
 
@@ -206,19 +233,8 @@ class _MedicationsState extends State<Medications> {
     List<String> days = ['S', 'M', 'T', 'W', 'Th', 'F', 'Sa'];
     List<Container> rowOfBoxes = new List<Container>(7);
 
-    if (columnBordered < 0) {
-      //will be -1 by default so the default selected is the given day
-
-      if (DateTime
-          .now()
-          .weekday == 7) {
-        columnBordered = 0;
-      } else {
-        columnBordered = DateTime
-            .now()
-            .weekday;
-      }
-    }
+    //selects what day should be bordered
+    columnBordered = selectedDayChecker(columnBordered);
 
     for (int i = 0; i < rowOfBoxes.length; i++) {
       if (i == columnBordered) {
@@ -281,11 +297,68 @@ class _MedicationsState extends State<Medications> {
     return rowOfBoxes;
   }
 
-  Table medInfoTable() {
-    return Table(
-      children: [
-        TableRow()
-      ],
+  //makes sure selectedDay != -1
+  int selectedDayChecker(int selectedDay) {
+    if (selectedDay < 0) {
+      if (DateTime
+          .now()
+          .weekday == 7) {
+        return 0;
+      } else {
+        return DateTime
+            .now()
+            .weekday;
+      }
+    }
+    else
+      return selectedDay;
+  }
+
+  Column medInfoList() {
+    List<MedicationInfo> medicationsForToday = new List<MedicationInfo>();
+    List<Card> medicationCards = new List<Card>();
+    //checks to make sure selectedDay != -1
+    int daySelected = selectedDayChecker(selectedDay);
+
+    for (int i = 0; i < medications.length; i++) {
+      for (int j = 0; j < medications[i].daysToTake.length; j++) {
+        if (medications[i].daysToTake[j] == daySelected) {
+          medicationsForToday.add(medications[i]);
+          String dose = medications[i].amount.toString() + ' ' +
+              medications[i].form;
+          medicationCards.add(medicationCard(medications[i].name, dose));
+        }
+      }
+    }
+
+
+    return Column(
+        children: medicationCards
     );
   }
 }
+
+Card medicationCard(String medication, String dose) {
+  return Card(
+    child: InkWell(
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.add_circle,
+            ),
+            Text(medication),
+            Text(dose),
+          ],
+        ),
+        margin: new EdgeInsets.symmetric(vertical: 15.0),
+        width: 500,
+      ),
+      onTap: () {
+        //_showModalBottomSheet(context);
+      },
+    ),
+  );
+}
+
