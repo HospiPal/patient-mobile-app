2529d1ff443cced9e6e95497d4485d622da827ab
justinfargnoli <justinfargnoli@gmail.com>
2021-01-14 12:43:57 -0500

    Allow Home to listen to MedicationsModel

--- lib/ui/central_screen/home/medications_for_today/medications_for_today.dart
+++ lib/ui/central_screen/home/medications_for_today/medications_for_today.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:patientapp/models/medications.dart';
+import 'package:provider/provider.dart';
 
 import 'medications_for_today_tile.dart';
 
 class MedicationsForToday extends StatelessWidget {
-  final List<MedicationsForTodayTile> appointments = <MedicationsForTodayTile>[
-    MedicationsForTodayTile("Dupixent", "50 Mg"),
-    MedicationsForTodayTile("Norethindrone", "20 Mg, Twice a day"),
-  ];
+  // final List<MedicationsForTodayTile> appointments = <MedicationsForTodayTile>[
+  //   MedicationsForTodayTile("Dupixent", "50 Mg"),
+  //   MedicationsForTodayTile("Norethindrone", "20 Mg, Twice a day"),
+  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +23,19 @@ class MedicationsForToday extends StatelessWidget {
                 //DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.4),
                 ),
           ),
-          getAppointmentCards(),
+          appointmentCards(context),
         ],
       ),
     );
   }
 
-  Widget getAppointmentCards() {
+  Widget appointmentCards(BuildContext context) {
+    final appointments = context
+        .select<MedicationsModel, List<MedicationModel>>(
+            (medicationsModel) => medicationsModel.medications)
+        .map((medicationModel) => MedicationsForTodayTile(
+            medicationModel.name, medicationModel.dose)).toList();
+
     if (appointments.length > 0) {
       return Column(children: appointments);
     } else {
